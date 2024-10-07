6ef6f1688b491e40070eb2df6faa7c1be2fb4e70
Walle-glitch <102384388+Walle-glitch@users.noreply.github.com>
2024-10-07 20:33:58 +0200

    Create bridge_wireshark.sh

---
+++ bridge_wireshark.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# List all network interfaces
+echo "Available network interfaces:"
+ip link show | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2}' | sed 's/ //g'
+
+# Prompt user to select two interfaces
+echo "Enter the first interface to bridge:"
+read iface1
+echo "Enter the second interface to bridge:"
+read iface2
+
+# Create the bridge interface
+echo "Creating the bridge between $iface1 and $iface2..."
+ip link add name br0 type bridge
+ip link set $iface1 master br0
+ip link set $iface2 master br0
+ip link set br0 up
+ip link set $iface1 up
+ip link set $iface2 up
+
+echo "Bridge created successfully. Starting Wireshark..."
+
+# Start Wireshark
+sudo wireshark &
+
+# Wait for Wireshark to close
+wait $!
+
+echo "Wireshark closed. Removing the bridge..."
+
+# Bring down the interfaces and remove the bridge
+ip link set $iface1 nomaster
+ip link set $iface2 nomaster
+ip link delete br0 type bridge
+
+echo "Bridge removed. Exiting."
